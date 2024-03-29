6a73c342e9a1b9bd46ef1428d8b093fe5240d1a6
ajoy-code-dreamer <ajoy.code.dreamer@gmail.com>
2024-03-29 15:26:40 +0600

    added counter live and counter section responsive

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// === jquery all operation ===
+$(function(){
+  $('.counter').counterUp({
+    delay: 10,
+    time: 1000
+});
+})
+// ==== counter js ====
+
+// ==== counter js ====
+// === jquery all operation ===
