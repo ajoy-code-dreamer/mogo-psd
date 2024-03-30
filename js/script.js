6ab35036582e640d50b4340638a658b234a53e2d
ajoy-code-dreamer <ajoy.code.dreamer@gmail.com>
2024-03-30 17:23:23 +0600

    slider section responsive done

--- js/script.js
+++ js/script.js
@@ -1,11 +1,48 @@
 // === jquery all operation ===
 $(function(){
+  // ==== counter js ====
   $('.counter').counterUp({
     delay: 10,
     time: 1000
 });
-})
 // ==== counter js ====
+// ==== slick slider js ====
+$('.slider_wrapper').slick({
+  infinite: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay:true,
+  prevArrow:'.slider_arrowLeft',
+  nextArrow:'.slider_arrowRight',
+  arrows: true,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+      }
+    },
+    {
+      breakpoint: 992,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+      }
+    },
+    {
+      breakpoint: 576,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+      }
+    }
+  ]
+});
+// ==== slick slider js ====
 
-// ==== counter js ====
+})
 // === jquery all operation ===
